a87b8c64fe836dc27c114f058066c3d6ba9b998f
Vitor - PC <vitor.email2011@gmail.com>
2023-08-15 09:36:15 -0300

    adaptações

---
+++ Service-Worker/wwwroot/js/registrar.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+debugger;
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("./service-worker.js")
+        .then(function (registration) {
+            // SUCESSO - ServiceWorker Registrado
+            console.log("ServiceWorker registrado com sucesso no escopo: ", registration.scope);
+        })
+        .catch(function (err) {
+            // ERRO - Falha ao registrar o ServiceWorker
+            console.log("Falha ao registrar o ServiceWorker: ", err);
+        });
+}
