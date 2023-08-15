128b457d2991dbf9fb2f91e5f8cd7f7d1ad6da1f
Vitor - PC <vitor.email2011@gmail.com>
2023-08-15 09:23:13 -0300

    projeto asp.net

---
+++ Service-Worker/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Service_Worker.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
