0f2ccd1f8000fb0050da4964c236d17dc2c88cf8
Vitor - PC <vitor.email2011@gmail.com>
2023-08-15 11:01:43 -0300

    Código no local certo

--- Service-Worker/wwwroot/js/registrar.js
+++ Service-Worker/wwwroot/js/registrar.js
@@ -4,19 +4,44 @@
 // Write your JavaScript code.
 
 debugger;
+let newWorker;
+
+// O evento de clique na notificação
+document.getElementById("reload").addEventListener("click", function () {
+    newWorker.postMessage({ action: "skipWaiting" });
+});
+
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("/js/service-worker.js")
+        .register("/js/service-worker.js") // [A]
         .then(function (registration) {
+            registration.addEventListener("updatefound", () => { // [B]
+                // Uma atualização no Service Worker foi encontrada, instalando...
+                newWorker = registration.installing; // [C]
+
+                newWorker.addEventListener("statechange", () => {
+                    // O estado do Service Worker mudou?
+                    switch (newWorker.state) {
+                        case "installed": {
+                            // Existe um novo Service Worker disponível, mostra a notificação
+                            if (navigator.serviceWorker.controller) {
+                                let notification = document.getElementById("notification");
+                                notification.className = "show";
+                                break;
+                            }
+                        }
+                    }
+                });
+            });
+
             // SUCESSO - ServiceWorker Registrado
-            console.log("ServiceWorker registrado com sucesso no escopo: ", registration.scope);
-            console.log("registration: ", registration);
-            console.log("navigator.serviceWorker: ", navigator.serviceWorker);
+            console.log(
+                "ServiceWorker registrado com sucesso no escopo: ",
+                registration.scope
+            );
         })
         .catch(function (err) {
             // ERRO - Falha ao registrar o ServiceWorker
             console.log("Falha ao registrar o ServiceWorker: ", err);
         });
-} else {
-    console.log("Navegador não aceite serviceWorker");
-};
+}
