69875f56bd8c64562885578de12ecdb187d11d3f
Vitor - PC <vitor.email2011@gmail.com>
2023-08-15 09:45:52 -0300

    logs e ajustes

--- Service-Worker/wwwroot/js/registrar.js
+++ Service-Worker/wwwroot/js/registrar.js
@@ -10,9 +10,13 @@ if ("serviceWorker" in navigator) {
         .then(function (registration) {
             // SUCESSO - ServiceWorker Registrado
             console.log("ServiceWorker registrado com sucesso no escopo: ", registration.scope);
+            console.log("registration: ", registration);
+            console.log("navigator.serviceWorker: ", navigator.serviceWorker);
         })
         .catch(function (err) {
             // ERRO - Falha ao registrar o ServiceWorker
             console.log("Falha ao registrar o ServiceWorker: ", err);
         });
-}
+} else {
+    console.log("Navegador não aceite serviceWorker");
+};
